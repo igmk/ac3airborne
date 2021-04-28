da857a4904ea17d61e2cd339b1f3090aeb9e16fa
Mario Mech <mario.mech@uni-koeln.de>
2021-04-28 10:13:06 +0200

    Changed file extension of intake catalogue.

--- ac3airborne/__init__.py
+++ ac3airborne/__init__.py
@@ -7,6 +7,7 @@ __version__ = get_versions()['version']
 del get_versions
 
 SEGMENT_REPO = "igmk/flight-phase-separation"
+# SEGMENT_REPO = "eurec4a/flight-phase-separation"
 
 
 class GithubApi:
@@ -31,9 +32,11 @@ def get_flight_segments(version="latest"):
         release_info = github.get(f"repos/{SEGMENT_REPO}/releases/latest")
     else:
         release_info = github.get(f"repos/{SEGMENT_REPO}/releases/tags/{version}")
+
     all_flights_asset = [a
                          for a in release_info["assets"]
                          if a["name"] == "all_flights.yaml"][0]
+    
     all_flights_url = all_flights_asset["browser_download_url"]
 
     return yaml.load(requests.get(all_flights_url).content, Loader=yaml.SafeLoader)
@@ -55,8 +58,7 @@ def get_intake_catalog():
     The catalog provides access to public AC3 airborne datasets without the need to
     manually specify URLs to the individual datasets.
     """
-    return intake.open_catalog("https://raw.githubusercontent.com/igmk/ac3airborne-intake/main/catalog.yml")
-    # return intake.open_catalog("https://raw.githubusercontent.com/eurec4a/eurec4a-intake/master/catalog.yml")
+    return intake.open_catalog("https://raw.githubusercontent.com/igmk/ac3airborne-intake/main/catalog.yaml")
 
 
 __all__ = ["get_flight_segments",
