e4fa5e7a311865cc73a546559682aca883db68f9
Giancarlo Marrollo <82207940+gmarrollo@users.noreply.github.com>
2021-11-18 20:05:48 +0100

    a lot of errors were corrected

--- ac3airborne/constants.py
+++ ac3airborne/constants.py
@@ -1,34 +1,34 @@
 # Settlements on Svalbard
 
 NYA = {
-  name: 'Ny-Ålesund',
-  lon: 11.922222,
-  lat: 78.925
+  'name': 'Ny-Ålesund',
+  'lon': 11.922222,
+  'lat': 78.925
 }
 
 LYB = {
-  name: 'Longyearbyen',
-  lon: 15.633333,
-  lat: 78.216667
+  'name': 'Longyearbyen',
+  'lon': 15.633333,
+  'lat': 78.216667
 }
 
 # Airports on Svalbard
 
 LYR = {
-  name: 'Svalbard Airport, Longyear',
-  icao: 'ENSB',
-  iata: 'LYR',
-  lon: 15.465556,
-  lat: 78.246111,
-  elev_m: 28.,
-  elev_ft: 94.
+  'name': 'Svalbard Airport, Longyear',
+  'icao': 'ENSB',
+  'iata': 'LYR',
+  'lon': 15.465556,
+  'lat': 78.246111,
+  'elev_m': 28.,
+  'elev_ft': 94.
 }
 
 ENAS = {
-  name: 'Ny-Ålesund Airport, Hamnerabben',
-  icao: 'ENAS',
-  lon: 11.874722,
-  lat: 78.927778,
-  elev_m: 40.,
-  elev_ft: 131.
+  'name': 'Ny-Ålesund Airport, Hamnerabben',
+  'icao': 'ENAS',
+  'lon': 11.874722,
+  'lat': 78.927778,
+  'elev_m': 40.,
+  'elev_ft': 131.
 }
