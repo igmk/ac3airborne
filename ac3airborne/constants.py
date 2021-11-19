87ef913b36355ab965b2981f9dfe7f4bfe66e531
Giancarlo Marrollo <giancarlo@giancarlo-desktop.localdomain>
2021-11-19 17:24:03 +0100

    Constants for Bremen and Kiruna Airport added

--- ac3airborne/constants.py
+++ ac3airborne/constants.py
@@ -20,8 +20,8 @@ LYR = {
   'iata': 'LYR',
   'lon': 15.465556,
   'lat': 78.246111,
-  'elev_m': 28.,
-  'elev_ft': 94.
+  'elev_m': 28,
+  'elev_ft': 94
 }
 
 ENAS = {
@@ -29,6 +29,28 @@ ENAS = {
   'icao': 'ENAS',
   'lon': 11.874722,
   'lat': 78.927778,
-  'elev_m': 40.,
-  'elev_ft': 131.
+  'elev_m': 40,
+  'elev_ft': 131
+}
+
+# Other locations
+
+BRE = {
+  'name': 'Bremen Airport',
+  'icao': 'EDDW',
+  'iata': 'BRE',
+  'lon': 8.786667,
+  'lat': 53.0475,
+  'elev_m': 4,
+  'elev_ft': 14
+}
+
+KRN = {
+  'name': 'Kiruna Airport',
+  'icao': 'ESNQ',
+  'iata': 'KRN',
+  'lon': 20.336667,
+  'lat': 67.822222,
+  'elev_m': 460,
+  'elev_ft': 1509,
 }
